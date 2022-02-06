f2cf3b6b64907ecb8cffc30e3bf133bc89992520
ElenaGabova <Eleyurgab@gmail.com>
2022-02-06 21:21:51 +0300

    Why I want to be a programmer

---
+++ CleanetCode.HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<string> stringList = new List<string>{
+"Я хочу создавать что-то новое",
+"Мне это интересно",
+"Дружное комьюнити",
+"Много возможностей для развития",
+"Это развивает логику, и помогает принимать более правильные решения" };
+string separator = "\n- ";
+string output = string.Format("Я хочу стать программистом потому-что:\n{0}", separator) + 
+                string.Join(separator, stringList);
+
+Console.WriteLine(output);
+Console.ReadLine();
